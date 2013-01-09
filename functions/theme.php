094ce6089fdfe849c859902056f10724a20c4807
Aldrich Allen Barcenas <pator@squirnet.co.cc>
2013-01-09 22:00:35 +0800

    excerpt function dup removed

--- functions/theme.php
+++ functions/theme.php
@@ -117,11 +117,6 @@
 		}
 	}
 	
-	function sbt_auto_excerpt_more( $more ) {
-		return ' ...';
-	}
-	add_filter( 'excerpt_more', 'sbt_auto_excerpt_more', 20 );
-	
 	function sbt_custom_excerpt_more( $output ) {
 		return preg_replace('/<a[^>]+>Continue reading.*?<\/a>/i','',$output);
 	}
